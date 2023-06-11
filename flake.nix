29704872c0712e167762f2ccc4804f2f84575ce8
ttak0422 <ttak0422@gmail.com>
2023-06-11 15:39:16 +0900

    refactor: rename

--- flake.nix
+++ flake.nix
@@ -52,6 +52,6 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems =
         [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      imports = [ ./flakes ./lib ./modules ];
+      imports = [ ./flakes ./libs ./modules ];
     };
 }
