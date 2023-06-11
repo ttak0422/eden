14ad12530dec8fa0cf00b7059b06a19c28d5337a
ttak0422 <ttak0422@gmail.com>
2023-06-11 22:28:54 +0900

    wip: add templates

--- flakes/overlay.nix
+++ flakes/overlay.nix
@@ -4,24 +4,34 @@
       inputs.neovim-nightly-overlay.overlay
       inputs.vim-plugins-overlay.overlay
       inputs.nix-filter.overlays.default
-      (final: prev: {
-        vimPlugins = prev.vimPlugins // {
-          # ddc-sorter_itemsize = prev.vimUtils.buildVimPluginFrom2Nix {
-          #   pname = "ddc-sorter_itemsize";
-          #   version = "local";
-          #   src = inputs.ddc-sorter_itemsize;
-          # };
-        };
-        tmuxPlugins = prev.tmuxPlugins // {
-          tmux-pomodoro-plus = prev.tmuxPlugins.mkTmuxPlugin {
-            pluginName = "tmux-pomodoro-plus";
-            rtpFilePath = "pomodoro.tmux";
-            version = "local";
-            src = inputs.tmux-pomodoro-plus;
+      (final: prev:
+        let inherit (prev.stdenv) mkDerivation;
+        in {
+          vimPlugins = prev.vimPlugins // {
+            # ddc-sorter_itemsize = prev.vimUtils.buildVimPluginFrom2Nix {
+            #   pname = "ddc-sorter_itemsize";
+            #   version = "local";
+            #   src = inputs.ddc-sorter_itemsize;
+            # };
           };
-        };
-        javaPackages = prev.javaPackages // { inherit (inputs) jol; };
-      })
+          tmuxPlugins = prev.tmuxPlugins // {
+            tmux-pomodoro-plus = prev.tmuxPlugins.mkTmuxPlugin {
+              pluginName = "tmux-pomodoro-plus";
+              rtpFilePath = "pomodoro.tmux";
+              version = "local";
+              src = inputs.tmux-pomodoro-plus;
+            };
+          };
+          javaPackages = prev.javaPackages // { inherit (inputs) jol; };
+          sonicCustomTemplates = mkDerivation {
+            name = "sonic-custom-templates";
+            src = ./../snippets/sonic;
+            installPhase = ''
+              mkdir $out
+              cp -r ./* $out
+            '';
+          };
+        })
     ];
   };
 }
