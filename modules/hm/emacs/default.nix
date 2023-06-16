5872b17af38f49eb78593ada4f226a1ece737ee9
ttak0422 <ttak0422@gmail.com>
2023-06-17 00:33:32 +0900

    refactor: split config to early-init

--- modules/hm/emacs/default.nix
+++ modules/hm/emacs/default.nix
@@ -2,7 +2,10 @@
   flake.nixosModules.eden-hm-emacs = { pkgs, ... }: {
     programs.emacs = {
       enable = true;
-      extraConfig = builtins.readFile ./../../../emacs/init.el;
+      extraConfig = ''
+        ${builtins.readFile ./../../../emacs/early-init.el}
+        ${builtins.readFile ./../../../emacs/init.el}
+      '';
     };
     home.file."emacs".text = "emacs";
   };
