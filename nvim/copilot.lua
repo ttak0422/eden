a6aafd9ca69a72d31fef50b1caba5c916d60fa04
ttak0422 <ttak0422@gmail.com>
2023-06-25 13:06:27 +0900

    feat(nvim): remap

--- nvim/copilot.lua
+++ nvim/copilot.lua
@@ -19,7 +19,7 @@ require("copilot").setup({
     auto_trigger = true,
     debounce = 150,
     keymap = {
-      accept = "<M-l>",
+      accept = "<M-y>",
       accept_word = false,
       accept_line = false,
       next = "<M-]>",
