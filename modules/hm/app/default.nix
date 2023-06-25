b7ff5cd92eb612f28feb48ff084f9767e8519813
ttak0422 <ttak0422@gmail.com>
2023-06-25 19:38:36 +0900

    refactor(nix): alias hack

--- modules/hm/app/default.nix
+++ modules/hm/app/default.nix
@@ -1,27 +1,34 @@
 { self, inputs, ... }: {
-  flake.nixosModules.eden-hm-app = { system, pkgs, config, lib, ... }: {
+  flake.nixosModules.eden-hm-app = { system, pkgs, config, lib, username, ... }: {
     imports = [ ./wezterm.nix ];
     home = {
-      packages = with pkgs;
-        [
-          # neovide
-        ];
-
       activation.aliasApplications = lib.mkIf pkgs.stdenv.hostPlatform.isDarwin
         (let
-          apps = pkgs.buildEnv {
+          hmApps = pkgs.buildEnv {
             name = "home-manager-applications";
             paths = config.home.packages;
             pathsToLink = "/Applications";
           };
+          # darwinApps = config.system.build.applications;
+            ## ${darwinApps}/Applications
           mkalias = inputs.mkAlias.outputs.apps.${system}.default.program;
         in lib.hm.dag.entryAfter [ "linkGeneration" ] ''
-          echo "Linking Home Manager applications..." 2>&1
+          applications="$HOME/Applications"
+          if ! test -d "$applications"; then
+              mkdir -p "$applications"
+              chown ${username}: "$applications"
+              chmod u+w "$applications"
+          fi
+
           app_path="$HOME/Applications/Home Manager Apps Hack"
-          tmp_path="$(mktemp -dt "home-manager-applications.XXXXXXXXXX")" || exit 1
-          ${pkgs.fd}/bin/fd \
-          -t l -d 1 . ${apps}/Applications \
-          -x $DRY_RUN_CMD ${mkalias} -L {} "$tmp_path/{/}"
+          tmp_path="$(mktemp -dt "home-manager-applications.XXXX")" || exit 1
+
+          find ${hmApps}/Applications -maxdepth 1 -type l 2>/dev/null | while IFS= read -r target_path
+          do
+            target_basename=$(basename $target_path)
+            $DRY_RUN_CMD ${mkalias} -L "$target_path" "$tmp_path/$target_basename"
+          done
+
           $DRY_RUN_CMD rm -rf "$app_path"
           $DRY_RUN_CMD mv "$tmp_path" "$app_path"
         '');
