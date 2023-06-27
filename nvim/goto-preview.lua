5ca39e3de3394773908a0167cb59bf2159728b85
ttak0422 <ttak0422@gmail.com>
2023-06-28 01:33:27 +0900

    refactor(nvim): modify goto-preview

--- nvim/goto-preview.lua
+++ nvim/goto-preview.lua
@@ -1,3 +1,7 @@
 require("goto-preview").setup({
   border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+  height = 30,
+  post_open_hook = function(_, win)
+    vim.api.nvim_win_set_option(win, "winhighlight", "Normal:")
+  end,
 })
