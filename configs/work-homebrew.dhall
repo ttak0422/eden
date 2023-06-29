caae5d695212f8076a8bd5f3909214f9d48e7cb0
ttak0422 <ttak0422@gmail.com>
2023-06-30 02:04:18 +0900

    wip

---
+++ configs/work-homebrew.dhall
@@ -0,0 +1,38 @@
+let fonts =
+      [ "font-fira-code"
+      , "font-hack-nerd-font"
+      , "font-hackgen-nerd"
+      , "font-jetbrains-mono-nerd-font"
+      , "font-plemol-jp"
+      , "font-plemol-jp-nf"
+      , "font-plemol-jp-hs"
+      , "font-roboto"
+      , "font-roboto-mono"
+      , "font-roboto-mono-nerd-font"
+      ]
+
+let browser = [ "firefox", "google-chrome" ]
+
+let term = [ "kitty" ]
+
+let develop = [ "tableplus", "visual-studio-code", "jetbrains-toolbox" ]
+
+in  { taps =
+      [ "homebrew/cask"
+      , "homebrew/cask-fonts"
+      , "homebrew/core"
+      , "koekeishiya/formulae"
+      ]
+    , brews = [ "lunchy", "qemu" ]
+    , casks =
+          [ "alfred"
+          , "aquaskk"
+          , "cyberduck"
+          , "displaylink"
+          , "karabiner-elements"
+          ]
+        # fonts
+        # browser
+        # term
+        # develop
+    }
--- modules/darwin/homebrew/default.nix
+++ modules/darwin/homebrew/default.nix
@@ -1,14 +1,28 @@
 { self, inputs, ... }: {
-  flake.nixosModules.eden-darwin-homebrew = { pkgs, ... }:
-    let
-      prefs =
-        pkgs.dhallToNix (builtins.readFile ./../../../configs/homebrew.dhall);
-    in {
-      homebrew = {
-        inherit (prefs) taps brews casks;
-        enable = true;
-        onActivation = { cleanup = "zap"; };
-        global = { brewfile = true; };
+  flake.nixosModules = {
+    eden-darwin-homebrew = { pkgs, ... }:
+      let
+        prefs =
+          pkgs.dhallToNix (builtins.readFile ./../../../configs/homebrew.dhall);
+      in {
+        homebrew = {
+          inherit (prefs) taps brews casks;
+          enable = true;
+          onActivation = { cleanup = "zap"; };
+          global = { brewfile = true; };
+        };
       };
-    };
+    eden-darwin-work-homebrew = { pkgs, ... }:
+      let
+        prefs = pkgs.dhallToNix
+          (builtins.readFile ./../../../configs/work-homebrew.dhall);
+      in {
+        homebrew = {
+          inherit (prefs) taps brews casks;
+          enable = true;
+          onActivation = { cleanup = "zap"; };
+          global = { brewfile = true; };
+        };
+      };
+  };
 }
