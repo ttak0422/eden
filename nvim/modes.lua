8f874bee52d0b6e70b236469b96389d08b40884b
ttak0422 <ttak0422@gmail.com>
2023-07-06 01:32:36 +0900

    chore(nvim): modify modes configs

--- nvim/modes.lua
+++ nvim/modes.lua
@@ -1,13 +1,11 @@
 require("modes").setup({
   colors = {
-    copy = "#f5c359",
-    delete = "#c75c6a",
     insert = "#78ccc5",
     visual = "#9745be",
   },
-  line_opacity = 0.35,
+  line_opacity = 0.15,
   set_cursor = true,
   set_cursorline = true,
-  set_number = true,
+  set_number = false,
   ignore_filetypes = dofile(args.exclude_ft_path),
 })
