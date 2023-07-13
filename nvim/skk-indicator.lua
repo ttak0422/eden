8140bf8cff5d1daed7b1735dab657ee3c255fc85
ttak0422 <ttak0422@gmail.com>
2023-07-14 01:27:27 +0900

    chore(nvim): modify skkeleton_indicator symbols

--- nvim/skk-indicator.lua
+++ nvim/skk-indicator.lua
@@ -2,4 +2,9 @@ require("skkeleton_indicator").setup({
   ignoreFt = {
     "TelescopePrompt",
   },
+  eijiText = "en",
+  hiraText = "jp",
+  kataText = "jp_kana",
+  hankataText = "jp_kana_han",
+  zenkakuText = "en_zen",
 })
