806bb212d53ba40beac18ed8290b5c50f3725fab
ttak0422 <ttak0422@gmail.com>
2023-07-22 16:22:38 +0900

    chore(nvim): set keymap

--- nvim/surround.lua
+++ nvim/surround.lua
@@ -1,1 +1,15 @@
-require("nvim-surround").setup()
+require("nvim-surround").setup({
+  keymaps = {
+    insert = "<C-g>s",
+    insert_line = "<C-g>S",
+    normal = "ys",
+    normal_cur = "yss",
+    normal_line = "yS",
+    normal_cur_line = "ySS",
+    visual = "Y",
+    visual_line = "gY",
+    delete = "ds",
+    change = "cs",
+    change_line = "cS",
+  },
+})
