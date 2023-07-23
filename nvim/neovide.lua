c65f6fb82acd724330e97e268d92a6fe52b9b0f0
ttak0422 <ttak0422@gmail.com>
2023-07-24 00:21:39 +0900

    chore: neovide

--- nvim/neovide.lua
+++ nvim/neovide.lua
@@ -8,5 +8,11 @@ local function change_scale_factor(delta)
   vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
 end
 
-vim.keymap.set("n", "<C-+>", function() change_scale_factor(1.25) end)
-vim.keymap.set("n", "<C-->", function() change_scale_factor(1/1.25) end)
+local delta = 1.1
+
+vim.keymap.set("n", "<C-+>", function()
+  change_scale_factor(delta)
+end)
+vim.keymap.set("n", "<C-->", function()
+  change_scale_factor(1 / delta)
+end)
