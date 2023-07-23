0d06fd684e98aba4b16cf8926067a1d006461eac
ttak0422 <ttak0422@gmail.com>
2023-07-23 20:27:32 +0900

    chore: neovide

--- nvim/neovide.lua
+++ nvim/neovide.lua
@@ -2,6 +2,7 @@ vim.o.guifont = "PlemolJP Console NF:h18 "
 vim.g.neovide_input_ime = false
 vim.g.neovide_cursor_animate_in_insert_mode = false
 vim.g.neovide_scale_factor = 1.0
+vim.g.neovide_input_macos_alt_is_meta = true
 
 local function change_scale_factor(delta)
   vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
