8e47c3225b3d01cf56b540c3a9f8aa24e83d69cc
ttak0422 <ttak0422@gmail.com>
2023-07-26 00:50:33 +0900

    feat: add neovide zoom toggle command

--- nvim/neovide.lua
+++ nvim/neovide.lua
@@ -1,18 +1,30 @@
+-- properties
 vim.o.guifont = "PlemolJP Console NF:h18 "
 vim.g.neovide_input_ime = false
 vim.g.neovide_cursor_animate_in_insert_mode = false
 vim.g.neovide_scale_factor = 1.0
 vim.g.neovide_input_macos_alt_is_meta = true
+vim.g.neovide_fullscreen = false
 
+local scale_delta = 1.1
+
+-- helper functions
 local function change_scale_factor(delta)
   vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
 end
 
-local delta = 1.1
+local toggle_zoom = (function()
+  return function()
+    vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen
+  end
+end)()
 
+-- apply
 vim.keymap.set("n", "<C-+>", function()
-  change_scale_factor(delta)
+  change_scale_factor(scale_delta)
 end)
 vim.keymap.set("n", "<C-->", function()
-  change_scale_factor(1 / delta)
+  change_scale_factor(1 / scale_delta)
 end)
+
+vim.api.nvim_create_user_command("ToggleNeovideFullScreen", toggle_zoom, {})
