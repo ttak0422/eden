53295c1443233fd14a6e4073cfd5788eb9ec21d3
ttak0422 <ttak0422@gmail.com>
2023-07-29 01:50:25 +0900

    chore(nvim/luasnip): expand or jump

--- nvim/luasnip.lua
+++ nvim/luasnip.lua
@@ -2,7 +2,9 @@ local ls = require("luasnip")
 local opts = { noremap = true, silent = true }
 
 vim.keymap.set({ "i", "s" }, "<C-k>", function()
-  ls.jump(1)
+  if ls.expand_or_jumpable() then
+    ls.expand_or_jump()
+  end
 end, opts)
 vim.keymap.set({ "i", "s" }, "<C-l>", function()
   ls.jump(-1)
