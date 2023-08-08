d70d75937b1628a94870a8e5bd53f60368972836
ttak0422 <ttak0422@gmail.com>
2023-08-09 01:46:13 +0900

    chore(nvim/tsnip): add typescript snippet

---
+++ snippets/tsnip/typescript.ts
@@ -0,0 +1,37 @@
+import type { Snippet } from "./deps.ts";
+
+const nonEmptyList: Snippet = {
+  name: "NonEmptyList",
+  params: [],
+  render: (_, { postCursor }) =>
+    `type NonEmptyList<T> = [T, ...T[]]${postCursor}`,
+};
+
+const nonEmptyGuard: Snippet = {
+  name: "NonEmpty Guard",
+  params: [],
+  render: (_, { postCursor }) => `
+    function nonEmpty<T>(list: T[]): list is NonEmptyList<T> {
+      return list.length > 0;
+    }${postCursor}
+  `,
+};
+
+const neverMatch: Snippet = {
+  name: "Never Match",
+  params: [
+    { name: "value",
+      type: "single_line",
+    }
+  ],
+  render: ({ value }, { postCursor }) => `
+    const never: never = ${value?.text ?? ""};
+    throw new Error(never);${postCursor}
+  `,
+}
+
+export default {
+  nonEmptyList,
+  nonEmptyGuard,
+  neverMatch,
+};
