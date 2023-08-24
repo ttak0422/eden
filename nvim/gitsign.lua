04af086128d4147b8785619fd6cfc7d07e063874
ttak0422 <ttak0422@gmail.com>
2023-08-25 01:01:30 +0900

    chore(nvim/gitsign): enable line blame

--- nvim/gitsign.lua
+++ nvim/gitsign.lua
@@ -15,4 +15,25 @@ require("gitsigns").setup({
     end
     return table.concat(status_txt, " ")
   end,
+  current_line_blame = true,
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = "eol",
+    delay = 1000,
+    ignore_whitespace = false,
+  },
+  current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+  sign_priority = 6,
+  update_debounce = 1000,
+  max_file_length = 40000,
+  preview_config = {
+    border = "single",
+    style = "minimal",
+    relative = "cursor",
+    row = 0,
+    col = 1,
+  },
+  yadm = {
+    enable = false,
+  },
 })
