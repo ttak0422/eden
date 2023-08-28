bbb3604458549d95b9689ebf25b3f1e385a98bb3
ttak0422 <ttak0422@gmail.com>
2023-08-29 01:40:53 +0900

    chore(nvim): modify pomodoro

--- nvim/piccolo-pomodoro.lua
+++ nvim/piccolo-pomodoro.lua
@@ -1,11 +1,49 @@
+local Type = require("piccolo-pomodoro.type")
+
+local prefix = {
+  [Type.TIMER_MODE.FOCUS] = {
+    [Type.TIMER_STATE.IDLE] = " focus",
+    [Type.TIMER_STATE.ACTIVE] = " focus",
+    [Type.TIMER_STATE.PAUSE] = " focus",
+  },
+  [Type.TIMER_MODE.BREAK] = {
+    [Type.TIMER_STATE.IDLE] = " break",
+    [Type.TIMER_STATE.ACTIVE] = " focus",
+    [Type.TIMER_STATE.PAUSE] = " focus",
+  },
+  [Type.TIMER_MODE.LONG_BREAK] = {
+    [Type.TIMER_STATE.IDLE] = " break",
+    [Type.TIMER_STATE.ACTIVE] = " break",
+    [Type.TIMER_STATE.PAUSE] = " break",
+  },
+}
+
 require("piccolo-pomodoro").setup({
   on_update = function()
     vim.cmd([[redrawstatus]])
   end,
+  focus_format = function(ctx)
+    return string.format("%s %02d:%02d", prefix[ctx.timer_mode][ctx.timer_state], ctx.m, ctx.s)
+  end,
+  break_format = function(ctx)
+    return string.format("%s %02d:%02d", prefix[ctx.timer_mode][ctx.timer_state], ctx.m, ctx.s)
+  end,
   on_complete_focus_time = function()
-    vim.notify("Focus time is over!")
+    if vim.fn.has("mac") == 1 then
+      vim.fn.system(
+        [[osascript -e 'display notification "Focus time is over!" with title "pomodoro" sound name "Bell"']]
+      )
+    else
+      vim.notify("Focus time is over!")
+    end
   end,
   on_complete_break_time = function()
-    vim.notify("Break time is over!")
+    if vim.fn.has("mac") == 1 then
+      vim.fn.system(
+        [[osascript -e 'display notification "Break time is over!" with title "pomodoro" sound name "Bell"']]
+      )
+    else
+      vim.notify("Focus time is over!")
+    end
   end,
 })
