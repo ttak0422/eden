914a072dd116d29b12a12f0bf06fa98159088d80
ttak0422 <ttak0422@gmail.com>
2023-08-30 01:11:15 +0900

    chore(nvim/pomodoro): addo on_start, on_pause

--- nvim/piccolo-pomodoro.lua
+++ nvim/piccolo-pomodoro.lua
@@ -28,6 +28,24 @@ require("piccolo-pomodoro").setup({
   break_format = function(ctx)
     return string.format("%s %02d:%02d", prefix[ctx.timer_mode][ctx.timer_state], ctx.m, ctx.s)
   end,
+  on_start = function()
+    if vim.fn.has("mac") == 1 then
+      vim.fn.system(
+        [[osascript -e 'display notification "Start!" with title "pomodoro" sound name "Bell"']]
+      )
+    else
+      vim.notify("Start!")
+    end
+  end,
+  on_pause = function()
+    if vim.fn.has("mac") == 1 then
+      vim.fn.system(
+        [[osascript -e 'display notification "Pause!" with title "pomodoro" sound name "Bell"']]
+      )
+    else
+      vim.notify("Pause!")
+    end
+  end,
   on_complete_focus_time = function()
     if vim.fn.has("mac") == 1 then
       vim.fn.system(
