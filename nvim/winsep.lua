141266f731c901492012b2134f35ce25fad193c1
ttak0422 <ttak0422@gmail.com>
2023-09-08 00:26:51 +0900

    chore(nvim): modify sep

--- nvim/winsep.lua
+++ nvim/winsep.lua
@@ -1,5 +1,16 @@
-require("colorful-winsep").setup({
+local win_sep = require("colorful-winsep")
+win_sep.setup({
   symbols = { "━", "┃", "┏", "┓", "┗", "┛" },
+  create_event = function()
+    local win_n = require("colorful-winsep.utils").calculate_number_windows()
+    if win_n == 2 then
+      local win_id = vim.fn.win_getid(vim.fn.winnr("h"))
+      local filetype = vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(win_id), "filetype")
+      if filetype == "NvimTree" then
+        win_sep.NvimSeparatorDel()
+      end
+    end
+  end,
 })
 -- reset
 vim.opt.laststatus = 3
