2dce2005e9530e97563bf96af0f7cc416b1a3e67
ttak0422 <ttak0422@gmail.com>
2023-09-13 02:05:09 +0900

    chore(darwin): add gui environment variables

--- modules/hm/darwin/default.nix
+++ modules/hm/darwin/default.nix
@@ -4,6 +4,8 @@ _: {
       # https://texwiki.texjp.org/?Mac#defaultkeybinding-dict
       file."/Library/KeyBindings/DefaultKeyBinding.dict".text =
         builtins.readFile ./../../../configs/DefaultKeyBinding.dict;
+      file."Library/LaunchAgents/Neovide.plist".text =
+        builtins.readFile ./../../../configs/Neovide.plist;
     };
   };
 }
