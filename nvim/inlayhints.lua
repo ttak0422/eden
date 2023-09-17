c8553c8de8d29015e603012f8d4a705b6f562f78
ttak0422 <ttak0422@gmail.com>
2023-09-17 17:10:16 +0900

    chore(nvim/inlayhints): modify opts

--- nvim/inlayhints.lua
+++ nvim/inlayhints.lua
@@ -1,1 +1,26 @@
-require("lsp-inlayhints").setup({})
+require("lsp-inlayhints").setup({
+  inlay_hints = {
+    parameter_hints = {
+      show = true,
+      prefix = "← ",
+      separator = ", ",
+      remove_colon_start = false,
+      remove_colon_end = true,
+    },
+    type_hints = {
+      show = true,
+      prefix = "",
+      separator = ", ",
+      remove_colon_start = false,
+      remove_colon_end = false,
+    },
+    only_current_line = false,
+    labels_separator = " ",
+    max_len_align = false,
+    max_len_align_padding = 1,
+    highlight = "LspInlayHint",
+    priority = 0,
+  },
+  enabled_at_startup = true,
+  debug_mode = false,
+})
