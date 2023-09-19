bb920d228d06f4eecf7171984e26c6cdaa67b3e1
ttak0422 <ttak0422@gmail.com>
2023-09-19 23:50:46 +0900

    chore(neovide): fix term keymap

--- nvim/neovide.lua
+++ nvim/neovide.lua
@@ -20,7 +20,7 @@ local toggle_zoom = (function()
 end)()
 
 -- apply
-vim.keymap.set({"n", "i", "c"}, "¥", "\\")
+vim.keymap.set({"n", "i", "c", "t"}, "¥", "\\")
 vim.keymap.set("n", "<C-+>", function()
   change_scale_factor(scale_delta)
 end)
