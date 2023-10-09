42a5fa391ba56126929bb8dc70d884925549fac2
ttak0422 <ttak0422@gmail.com>
2023-10-10 01:52:54 +0900

    chore(nvim): disable treesitter highlight nix

--- nvim/treesitter.lua
+++ nvim/treesitter.lua
@@ -9,6 +9,9 @@ require("nvim-treesitter.configs").setup({
     enable = true,
     additional_vim_regex_highlighting = false,
     disable = function(lang, buf)
+      if lang == "nix" then
+        return true
+      end
       local max_filesize = 100 * 1024 -- 100 KB
       local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
       if ok and stats and stats.size > max_filesize then
