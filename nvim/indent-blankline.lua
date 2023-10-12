66b99e2a64480c49cf395f318a6149ed3025c8f1
ttak0422 <ttak0422@gmail.com>
2023-10-13 01:35:44 +0900

    chore(indent-blankline): → `ibl`

--- nvim/indent-blankline.lua
+++ nvim/indent-blankline.lua
@@ -1,9 +1,27 @@
 vim.opt.list = true
-require("indent_blankline").setup({
-  show_end_of_line = true,
-  space_char_blankline = " ",
-  use_treesitter = true,
-  show_current_context = true,
-  show_current_context_start = false,
-  filetype_exclude = dofile(args.exclude_ft_path),
+require("ibl").setup({
+  debounce = 500,
+  viewport_buffer = { min = 100, max = 600 },
+  indent = {
+    char = "|",
+    smart_indent_cap = true,
+  },
+  whitespace = {
+    remove_blankline_trail = true,
+  },
+  scope = {
+    -- defined in rainbow-delimiters
+    highlight = {
+      -- "RainbowDelimiterRed",
+      "RainbowDelimiterYellow",
+      "RainbowDelimiterBlue",
+      "RainbowDelimiterOrange",
+      "RainbowDelimiterGreen",
+      "RainbowDelimiterViolet",
+      "RainbowDelimiterCyan",
+    },
+  },
+})
+require("ibl").overwrite({
+  exclude = { filetypes = dofile(args.exclude_ft_path) },
 })
