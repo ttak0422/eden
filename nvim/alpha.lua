3f18da5b2aee9fe8b2ffe4facbe65dd6f47c990f
ttak0422 <ttak0422@gmail.com>
2023-10-20 00:19:19 +0900

    chore(nvim): modify alpha

--- nvim/alpha.lua
+++ nvim/alpha.lua
@@ -1,15 +1,20 @@
 local alpha = require("alpha")
 local dashboard = require("alpha.themes.dashboard")
-dashboard.section.header.val = [[
+dashboard.section.header.val = {
+  [[                                               ]],
+  [[                                               ]],
+  [[    ██████████████ ███████████   ██   ]],
+  [[   ██     ██  ████     █████  ██    ]],
+  [[  █████████  ██████████████ ██     ]],
+  [[ ██     ██  ████     ██ █████      ]],
+  [[████████████████████████  █████       ]],
+  [[                                               ]],
+  [[                                               ]],
+}
 
-
-    ██████████████ ███████████   ██
-   ██     ██  ████     █████  ██
-  █████████  ██████████████ ██
- ██     ██  ████     ██ █████
-████████████████████████  █████
-
-
-]]
+dashboard.section.buttons.val = {
+  dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+  dashboard.button("q", "  Quit Nvim", ":qa<CR>"),
+}
 
 alpha.setup(dashboard.opts)
