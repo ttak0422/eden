a85b69ea0a95191dbd6a5970fe4232f97adc2792
ttak0422 <ttak0422@gmail.com>
2023-10-25 23:37:53 +0900

    wip(nvim/ddu): set nobuflisted to ddu preview

--- nvim/ddu.ts
+++ nvim/ddu.ts
@@ -41,6 +41,7 @@ export class Config extends BaseConfig {
       }) => {
         await fn.win_execute(args.denops, args.previewWinId, "normal! zz");
         await fn.win_execute(args.denops, args.previewWinId, "setlocal nu");
+        await fn.win_execute(args.denops, args.previewWinId, "set nobuflisted");
       },
       winRow: "&lines / 3 * 2",
       winCol: 1,
