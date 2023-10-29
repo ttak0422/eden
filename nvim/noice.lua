6d8d7029dd26443f94502aba9b75ed0595eef26b
ttak0422 <ttak0422@gmail.com>
2023-10-29 22:11:24 +0900

    chore(nvim/noice): modify routes filter

--- nvim/noice.lua
+++ nvim/noice.lua
@@ -138,6 +138,21 @@ require("noice").setup({
       },
     },
   },
+  routes = {
+    {
+      filter = {
+        event = "msg_show",
+        any = {
+          { find = "%d+L, %d+B" },
+          { find = "; after #%d+" },
+          { find = "; before #%d+" },
+          { find = "%d fewer lines" },
+          { find = "%d more lines" },
+        },
+      },
+      opts = { skip = true },
+    },
+  },
 })
 
 vim.keymap.set({ "n", "i", "s" }, "<C-f>", function()
