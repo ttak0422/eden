82133a170bbdc8d2d6bde03950628b39f151d0a9
ttak0422 <ttak0422@gmail.com>
2023-11-08 22:53:45 +0900

    chore(nvim/wf): change wf key

--- nvim/fnl/wf.fnl
+++ nvim/fnl/wf.fnl
@@ -2,15 +2,15 @@
 
 (local which_key (require :wf.builtin.which_key))
 ;; <Leader> is <Space>
-(vim.keymap.set [:n] :<Space> (which_key {:text_insert_in_advance :<Space>})
+(vim.keymap.set [:n] :<Space><Space> (which_key {:text_insert_in_advance :<Space>})
                 {:noremap true :silent true})
 
 ;; fuzzy finder
-(vim.keymap.set [:n] :<Space>f (which_key {:text_insert_in_advance :<Space>f})
+(vim.keymap.set [:n] :<Space><Space>f (which_key {:text_insert_in_advance :<Space>f})
                 {:noremap true :silent true})
 
 ;; git
-(vim.keymap.set [:n] :<Space>g (which_key {:text_insert_in_advance :<Space>g})
+(vim.keymap.set [:n] :<Space><Space>g (which_key {:text_insert_in_advance :<Space>g})
                 {:noremap true :silent true})
 
 nil
--- nvim/lua/wf.lua
+++ nvim/lua/wf.lua
@@ -1,7 +1,7 @@
 -- [nfnl] Compiled from nvim/fnl/wf.fnl by https://github.com/Olical/nfnl, do not edit.
 do end (require("wf")).setup({theme = "space"})
 local which_key = require("wf.builtin.which_key")
-vim.keymap.set({"n"}, "<Space>", which_key({text_insert_in_advance = "<Space>"}), {noremap = true, silent = true})
-vim.keymap.set({"n"}, "<Space>f", which_key({text_insert_in_advance = "<Space>f"}), {noremap = true, silent = true})
-vim.keymap.set({"n"}, "<Space>g", which_key({text_insert_in_advance = "<Space>g"}), {noremap = true, silent = true})
+vim.keymap.set({"n"}, "<Space><Space>", which_key({text_insert_in_advance = "<Space>"}), {noremap = true, silent = true})
+vim.keymap.set({"n"}, "<Space><Space>f", which_key({text_insert_in_advance = "<Space>f"}), {noremap = true, silent = true})
+vim.keymap.set({"n"}, "<Space><Space>g", which_key({text_insert_in_advance = "<Space>g"}), {noremap = true, silent = true})
 return nil
