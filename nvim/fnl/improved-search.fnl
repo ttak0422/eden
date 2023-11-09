cc6350beedf3610103c140eb54dd51a1fa53831c
ttak0422 <ttak0422@gmail.com>
2023-11-09 13:32:41 +0900

    fixup

--- nvim/fnl/improved-search.fnl
+++ nvim/fnl/improved-search.fnl
@@ -2,7 +2,7 @@
 
 (vim.keymap.set [:n :x :o] :n search.stable_next)
 (vim.keymap.set [:n :x :o] :N search.stable_previous)
-(vim.keymap.set [:x] :* search.forward)
-(vim.keymap.set [:x] :# search.backward)
+(vim.keymap.set [:x] :* search.in_place)
+(vim.keymap.set [:x] :# search.in_place)
 
 nil
--- nvim/lua/improved-search.lua
+++ nvim/lua/improved-search.lua
@@ -2,6 +2,6 @@
 local search = require("improved-search")
 vim.keymap.set({"n", "x", "o"}, "n", search.stable_next)
 vim.keymap.set({"n", "x", "o"}, "N", search.stable_previous)
-vim.keymap.set({"x"}, "*", search.forward)
-vim.keymap.set({"x"}, "#", search.backward)
+vim.keymap.set({"x"}, "*", search.in_place)
+vim.keymap.set({"x"}, "#", search.in_place)
 return nil
