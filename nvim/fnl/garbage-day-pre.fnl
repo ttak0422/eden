ae2fb31396652c3742e67650ea18b0105df3f9de
ttak0422 <ttak0422@gmail.com>
2023-11-14 00:11:44 +0900

    fix: garbage-day

--- nvim/fnl/garbage-day-pre.fnl
+++ nvim/fnl/garbage-day-pre.fnl
@@ -1,4 +1,4 @@
-(tset vim.g.garbage_day_config :aggressive_mode false)
-(tset vim.g.garbage_day_config :excluded_lsp_clients [:null-ls :jdtls])
-(tset vim.g.garbage_day_config :grace_period 900)
-(tset vim.g.garbage_day_config :wakeup_delay 0)
+(set vim.g.garbage_day_config {:aggressive_mode false
+                               :excluded_lsp_clients [:null-ls :jdtls]
+                               :grace_period 900
+                               :wakeup_delay 0})
--- nvim/lua/garbage-day-pre.lua
+++ nvim/lua/garbage-day-pre.lua
@@ -1,6 +1,3 @@
 -- [nfnl] Compiled from nvim/fnl/garbage-day-pre.fnl by https://github.com/Olical/nfnl, do not edit.
-vim.g.garbage_day_config["aggressive_mode"] = false
-vim.g.garbage_day_config["excluded_lsp_clients"] = {"null-ls", "jdtls"}
-vim.g.garbage_day_config["grace_period"] = 900
-vim.g.garbage_day_config["wakeup_delay"] = 0
+vim.g.garbage_day_config = {excluded_lsp_clients = {"null-ls", "jdtls"}, grace_period = 900, wakeup_delay = 0, aggressive_mode = false}
 return nil
