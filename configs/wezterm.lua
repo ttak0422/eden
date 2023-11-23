971a70152e4dcb2990434a8045ead435c761c484
ttak0422 <ttak0422@gmail.com>
2023-11-24 01:00:05 +0900

    fixtypo

--- configs/wezterm.lua
+++ configs/wezterm.lua
@@ -3,4 +3,4 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 local keys = {{key = "=", mods = "CTRL", action = act.IncreaseFontSize}, {key = "Enter", mods = "ALT", action = act.DisableDefaultAssignment}, {key = "f", mods = "SHIFT|SUPER", action = act.ToggleFullScreen}}
 local window_padding = {left = 5, right = 5, top = 5, bottom = 0}
-return {color_scheme = "Everforest Dark (Gogh)", font = wezterm.font_with_fallback({["PlemolJP Console NF"] = "JetBrains Mono", ["Noto Color Emoji"] = "Symbols Nerd Font Mono"}), allow_square_glyphs_to_overflow_width = "WhenFollowedBySpace", window_decorations = "RESIZE", keys = keys, window_padding = window_padding, enable_tab_bar = false, adjust_window_size_when_changing_font_size = false}
+return {color_scheme = "Everforest Dark (Gogh)", font = wezterm.font_with_fallback({"PlemolJP Console NF", "JetBrains Mono", "Noto Color Emoji", "Symbols Nerd Font Mono"}), allow_square_glyphs_to_overflow_width = "WhenFollowedBySpace", window_decorations = "RESIZE", keys = keys, window_padding = window_padding, adjust_window_size_when_changing_font_size = false, enable_tab_bar = false}
