7a0bbf473c45f4628dc02aee18ca112ee4468cd7
ttak0422 <ttak0422@gmail.com>
2023-11-26 22:44:30 +0900

    fix(nvim): bufresize trigger events

--- nvim/fnl/bufresize.fnl
+++ nvim/fnl/bufresize.fnl
@@ -1,6 +1,4 @@
 (let [bufresize (require :bufresize)
-      register {:keys [] :trigger_events [:BufWinEnter :WinEnter :WinNew]}
-      resize {:keys []
-              :trigger_events [:VimResized :WinClosed]
-              :increment false}]
+      register {:keys [] :trigger_events [:BufWinEnter :WinEnter]}
+      resize {:keys [] :trigger_events [:VimResized] :increment false}]
   (bufresize.setup {: register : resize}))
--- nvim/lua/bufresize.lua
+++ nvim/lua/bufresize.lua
@@ -1,5 +1,5 @@
 -- [nfnl] Compiled from nvim/fnl/bufresize.fnl by https://github.com/Olical/nfnl, do not edit.
 local bufresize = require("bufresize")
-local register = {keys = {}, trigger_events = {"BufWinEnter", "WinEnter", "WinNew"}}
-local resize = {keys = {}, trigger_events = {"VimResized", "WinClosed"}, increment = false}
+local register = {keys = {}, trigger_events = {"BufWinEnter", "WinEnter"}}
+local resize = {keys = {}, trigger_events = {"VimResized"}, increment = false}
 return bufresize.setup({register = register, resize = resize})
