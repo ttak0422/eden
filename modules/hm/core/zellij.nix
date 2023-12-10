be85b432513442ea92f8573ab4a181e442c6445a
ttak0422 <ttak0422@gmail.com>
2023-12-10 21:43:12 +0900

    wip(zellij): minimal

--- modules/hm/core/zellij.nix
+++ modules/hm/core/zellij.nix
@@ -1,3 +1,7 @@
 { config, pkgs, lib, ... }: {
   home.packages = with pkgs.pkgs-unstable; [ zellij ];
+  home.file.".config/zellij/config.kdl".text =
+    builtins.readFile ./../../../configs/zellij/config.kdl;
+  home.file.".config/zellij/layouts/default.kdl".text =
+    builtins.readFile ./../../../configs/zellij/layouts/default.kdl;
 }
